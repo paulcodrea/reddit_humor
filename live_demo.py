4d1c25a06b3d0355a368d0ec6f9f97aa152de3a8
Paul CODREA <paulcodrea@users.noreply.github.com>
2022-05-10 13:31:46 +0100

    Update live demo to read and process random forest model

--- live_demo.py
+++ live_demo.py
@@ -9,8 +9,9 @@ import pandas as pd
 from nltk.tokenize import word_tokenize
 
 from keras.models import load_model
-# from keras.preprocessing.text import Tokenizer
 from keras.preprocessing.sequence import pad_sequences
+
+from sklearn.ensemble import RandomForestClassifier
 
 class humour_live_demo:
     def __init__(self, path):
@@ -61,7 +62,7 @@ class humour_live_demo:
 path = 'C:\\Users\\paulc\\OneDrive\\Desktop\\NLU_humour\\reddit_humor\\model\\'
 live_session = humour_live_demo(path)
 
-
+# print("______________________________________________________________")
 # Strart the model here. Read input from user one line at a time.
 while True:
     ret = input("Enter a sentence: ") # Read input from user
@@ -69,7 +70,7 @@ while True:
     # Check if the user wants to exit
     if ret == "exit":
         break
-
+    print("\n______________________________________________________________")
     # Process the input for each of the models.
     for file in os.listdir(live_session.path):
         if file.endswith(".h5"):
@@ -78,8 +79,7 @@ while True:
             # 1. Read Model
             try: 
                 live_session.model = load_model(live_session.path + file)
-                print("\n______________________________________________________________")
-                print("Model loaded: " + file)
+                print("\n\nModel loaded: " + file)
             except Exception as e:
                 print(e)
                 print("Model not loaded")
@@ -108,8 +108,46 @@ while True:
             # 4. Process the input
             live_session.process_input(ret)
             live_session.run_model()
-            print("______________________________________________________________")
-            print("\n")
+
+        # THIS IS FOR RANDOM FOREST ---------------------------------------------------------------------------------
+        elif file.startswith("3_model"):
+            model_name = file[:1]
+            # 1. Read Model
+            try: 
+                with open(live_session.path + model_name + '_model.pickle', 'rb') as handle:
+                    live_session.model = pickle.load(handle)
+                print("\n\nModel loaded: " + file)
+            except Exception as e:
+                print(e)
+                print("Model not loaded")
+                continue
+
+            # 2. Read Data 
+            try:
+                live_session.data_df = pd.read_csv(live_session.path + model_name + '_data.csv')
+                print("Data loaded: " + model_name + '_data.csv')
+            except:
+                print("No data found")
+                continue
+
+            # 3. Read Pickle file
+            try:
+                with open(live_session.path + model_name + '_tokenizer.pickle', 'rb') as handle:
+                    live_session.tokenizer = pickle.load(handle)
+                print("Tokenizer loaded: " + model_name + '_tokenizer.pickle')
+            except:
+                print("No tokenizer found")
+                continue
+            
+            # Save Max Length
+            live_session.max_length = live_session.data_df['max_len'][0]
+
+            # 4. Process the input
+            live_session.process_input(ret)
+            live_session.run_model()
+
+    print("______________________________________________________________")
+    print("\n")
 
 
 print("End of model...")
